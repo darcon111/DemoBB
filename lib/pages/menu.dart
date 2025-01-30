beb343adf5f88ea38a2da0bbc93dfac056e17b34
darcon111 <darcon111@hotmail.com>
2025-01-30 13:49:00 -0500

    cambios

--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../shared/item_banner.dart';
+import '../shared/item_product.dart';
+import '../shared/toolbar.dart';
+
 class MenuPage extends StatefulWidget {
   @override
   _MenuPageState createState() => _MenuPageState();
@@ -7,14 +11,55 @@ class MenuPage extends StatefulWidget {
 
 class _MenuPageState extends State<MenuPage> {
 
-  int counter = 10;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children:[],
+        children:[
+        Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(color: Colors.white),
+          child:
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 16,
+              children: [
+                TopAppBar(),
+                ItemBanner(),
+                Column(
+                  children: [
+                    Container(
+                      width: 360,
+                      height: 36,
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Mis productos',
+                            style: TextStyle(
+                              color: Color(0xFF212529),
+                              fontSize: 16,
+                              fontFamily: 'Lexend',
+                              fontWeight: FontWeight.w500,
+                              height: 1.50,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ]
+            )
+          ,
+        ),
+        ],
       ),
     );
   }
